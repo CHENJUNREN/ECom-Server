b74e761404a7538310c35ae881fb27a203840716
Chenjun <tonyren@my.yorku.ca>
2021-01-27 17:57:10 -0500

    changed session config

--- index.js
+++ index.js
@@ -31,10 +31,7 @@ app.use(
 		resave: "false",
 		saveUninitialized: "false",
 		cookie: {
-			httpOnly: false,
-			maxAge: null,
-			// allow the cookie to be sent via HTTP ("true" means "HTTPS only)
-			secure: false,
+			secure: true,
 			sameSite: "none",
 		},
 	})
